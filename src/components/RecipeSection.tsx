b155c1658796eda428d7fe3572dcc1adfbe181cb
Michal Gajda <mgajda36@gmail.com>
2026-03-26 21:22:58 +0100

    feat: created two components

---
+++ src/components/RecipeSection.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+
+type Props = {
+  header: string;
+  children: ReactNode;
+};
+
+export default function RecipeSection({ header, children }: Props) {
+  return (
+    <section className="recipe__section">
+      <h2>{header}</h2>
+      {children}
+    </section>
+  );
+}
